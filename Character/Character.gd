000206b3a3d1cae075d3b2495e00b16fcaf3e7aa
unknown <luccornu.tf@outlook.fr>
2023-07-08 03:22:08 +0200

    First draft for turnbase system

--- Character/Character.gd
+++ Character/Character.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+class_name Character
+
 enum CharacterClass
 {
 	GUERRIER,
@@ -30,3 +32,14 @@ var character_class
 var character_state
 var character_action
 
+func _ready():
+	pass
+
+func _process(_delta: float):
+	pass
+
+func _physics_process(_delta: float):
+	pass
+
+func do_action():
+	pass
