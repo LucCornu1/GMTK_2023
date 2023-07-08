2d9f4a63fab4d4f582514f82aeba7e4e81b67189
Irthir <romain.schlotter@gmail.com>
2023-07-08 11:49:48 +0200

    Mise en place du choix de l'action des personnages.

--- Character/Character.gd
+++ Character/Character.gd
@@ -7,8 +7,6 @@ enum CharacterClass
 	GUERRIER,
 	MAGE,
 	VOLEUR,
-	PRETRE,
-	ARTIFICIER,
 	GOBELIN,
 	ORC,
 	SQUELETTE
@@ -32,6 +30,27 @@ var character_class : CharacterClass
 var character_state : CharacterState
 var character_action : CharacterAction
 
+var GuerrierAction = [
+	[0.5,0.25,0.25,0], #Neutre
+	[0.7,0.3,0,0], #Colère
+	[0.1,0,0.9,0], #Peur
+	[0.2,0.7,0,0.1], #Confiance
+	[0.2,0,0,0.8]] #Ennui
+
+var MageAction = [
+	[0.5,0.2,0.1,0.2], #Neutre
+	[0.4,0,0.6,0], #Colère
+	[0,0.8,0,0.2], #Peur
+	[0.8,0,0.1,0.1], #Confiance
+	[0.1,0.1,0,0.8]] #Ennui
+
+var VoleurAction = [
+	[0.6,0.2,0.2,0], #Neutre
+	[0.8,0.2,0,0], #Colère
+	[0,0.1,0.8,0.1], #Peur
+	[0.4,0.6,0,0], #Confiance
+	[0.2,0,0,0.8]] #Ennui
+
 #Mutateurs
 func SetClass(_class : CharacterClass):
 	character_class = _class
@@ -52,8 +71,34 @@ func GetState() -> CharacterState:
 func GetAction() -> CharacterAction:
 	return character_action
 
+#Fonctions membres
+
+#Retourne l'action choisie par la classe du personnage et la sauvegarde.
 func ChoseAction(_class : CharacterClass = character_class, _state : CharacterState = character_state) -> CharacterAction :
-	return character_action
+	var _action : CharacterAction
+	var _tabAction
+	match _class:
+		CharacterClass.GUERRIER :
+			_tabAction = GuerrierAction
+		CharacterClass.MAGE :
+			_tabAction = MageAction
+		CharacterClass.VOLEUR :
+			_tabAction = VoleurAction
+	var _fRand : float = randf()
+	
+	if (_fRand < _tabAction[0]) : 
+		_action = CharacterAction.ACTION1
+	elif (_fRand < (_tabAction[0]+_tabAction[1])) :
+		_action = CharacterAction.ACTION2
+	elif (_fRand < (_tabAction[0]+_tabAction[1]+_tabAction[2])) :
+		_action = CharacterAction.ACTION3
+	else :
+		_action = CharacterAction.ATTENDRE
+	
+	if (_state == character_state && _class == character_class) : #Sauvegarde de l'action uniquement si les opérateurs n'ont pas été surchargés
+		character_action = _action
+	
+	return character_action #Retourne l'action choisie par la classe du personnage et la sauvegarde.
 
 func _ready():
 	pass
